ca7ff091e49fadfb54d64a0980fddcb4ab88ac62
Tom Smith <tom.smith@okta.com>
2019-11-13 17:58:47 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+///////////////////////////////////////////////////
+// Okta terraform frontend
+
+require('dotenv').config()
+
+var bodyParser = require('body-parser')
+
+const express = require('express')
+
+///////////////////////////////////////////////////
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
+
+app.use(express.static('public'))
+
+require('./routes.js')(app);
+
+var port = process.env.PORT || 3877;
+
+app.listen(port, function () {
+	console.log('App listening on port ' + port);
+});
---
+++ routes.js
@@ -0,0 +1,108 @@
+
+const { exec } = require('child_process')
+
+var fs = require('fs')
+
+var request = require('request')
+
+///////////////////////////////////////////////////
+
+module.exports = function (app) {
+
+	app.post('/apply', function(req, res, next) {
+
+		console.log("the request body is: ")
+		console.dir(req.body)
+
+		// I've checked for the presence of all values
+		// on the front-end
+
+		check_api_key(req, res, function(err) {
+			if (err) {
+				console.log(err)
+
+				var error_obj = {}
+
+				error_obj.error = 'api_token_error'
+				error_obj.error_msg = 'something went wrong testing the url + API token combination, so we haven\'t tried to do anything with terraform.',
+				error_obj.url = 'https://' + req.body.okta_org_name + '.' + req.body.okta_base_url
+				error_obj.okta_api_token = req.body.okta_api_token.substr(0, 3) + '...'
+
+				res.json(error_obj)
+
+				return
+			}
+
+			var options = {
+				uri: process.env.tf_backend + '/apply',
+				method: 'POST',
+				json: req.body
+			}
+
+			request(options, function (error, response, body) {
+
+				console.dir(response)
+
+				if (!error && response.statusCode == 200) {
+
+					var response_obj = {}
+
+					response_obj.msg = 'terraform_success'
+					response_obj.body = body
+
+					res.json(response_obj)
+
+					return
+				}
+				else {
+
+					console.dir(error)
+
+					var error_obj = {}
+
+					error_obj.error = 'terraform_error'
+					error_obj.error_msg = 'something went wrong with the terraform operation'
+
+					res.json(error_obj)
+
+					return
+				}
+			})
+		})
+	})
+}
+
+function check_api_key(req, res, callback) {
+
+	var url = 'https://' + req.body.okta_org_name + '.' + req.body.okta_base_url
+
+	var options = {
+		method: 'GET',
+		url: url + '/api/v1/meta/schemas/user/default',
+		headers: {
+			Authorization: 'SSWS ' + req.body.okta_api_token,
+			'Content-Type': 'application/json',
+			Accept: 'application/json'
+		}
+	}
+
+	request(options, function (error, response, body) {
+		if (error) throw new Error(error)
+
+		console.log("the statusCode is: " + response.statusCode)
+
+		if (response.statusCode === 200) {
+			return callback(null)
+		}
+		else {
+			var obj = {
+				error: "api_token_error",
+				error_msg: "something went wrong testing the url + API token combination, so we haven't tried to do anything with terraform.",
+				url: url,
+				api_token: req.body.okta_api_token.substring(0,4) + "..."
+			}
+
+			return callback(obj)
+		}
+	})
+}
